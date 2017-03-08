0c4e43a9f179147060ec1e6e625e131b8d48cabb
MikeB <burton.mj@gmail.com>
2017-03-07 19:29:07 -0700

    finished both branches with random messages working

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ var express = require('express');
 var app = express();
 
 var getRandomMessage = function(message){
-  return 'hello';
+
+  var rand = messages[Math.floor(Math.random() * messages.length)];
+  return rand;
+
 }
 
 
@@ -14,7 +17,28 @@ app.listen(8887, function(){
 });
 
 app.get('/', function(req, res) {
-  res.send(JSON.stringify({
+  res.status(200).set({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'X-XSS-Protection': '1; mode=block',
+    'X-Frame-Options': 'SAMEORIGIN',
+    'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+  }).send(JSON.stringify({
     message: getRandomMessage()
   }))
+});
+
+
+app.options('/', function(req, res) {
+  res.status(200).set({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'X-XSS-Protection': '1; mode=block',
+    'X-Frame-Options': 'SAMEORIGIN',
+    'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+  }).send();
 });
